67424e68fd6707b7ae29ee1f0431f3177e161d5e
fengsh <shaohong.feng.78@gmail.com>
2024-05-02 08:33:33 -0400

    introduce chatgpt 4.0

--- extractor/request_openai.py
+++ extractor/request_openai.py
@@ -7,21 +7,29 @@ logger = logging.getLogger(__name__)
 
 openai_type = os.environ.get("OPENAI_API_TYPE")
 if openai_type == "azure":
-    client = AzureOpenAI(
-        azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT", None),
-        api_key=os.environ.get("OPENAI_API_KEY", None),
-        api_version=os.environ.get("OPENAI_API_VERSION", None),
+    client_35 = AzureOpenAI(
+        azure_endpoint=os.environ.get("AZURE_OPENAI_35_ENDPOINT", None),
+        api_key=os.environ.get("OPENAI_35_API_KEY", None),
+        api_version=os.environ.get("OPENAI_35_API_VERSION", None),
     )
-    model = os.environ.get("OPENAI_DEPLOYMENT_NAME", None)
+    model_35 = os.environ.get("OPENAI_35_DEPLOYMENT_NAME", None)
+    client_40 = AzureOpenAI(
+        azure_endpoint=os.environ.get("AZURE_OPENAI_40_ENDPOINT", None),
+        api_key=os.environ.get("OPENAI_40_API_KEY", None),
+        api_version=os.environ.get("OPENAI_40_API_VERSION", None),
+    )
+    model_40 = os.environ.get("OPENAI_40_DEPLOYMENT_NAME", None)
 else:
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", None))
-    model = "gpt-3.5-turbo" # "gpt-4-1106-preview"
+    client_35 = OpenAI(api_key=os.environ.get("OPENAI_35_API_KEY", None))
+    model_35 = "gpt-3.5-turbo" # "gpt-4-1106-preview"
+    client_40 = OpenAI(api_key=os.environ.get("OPENAI_40_API_KEY", None))
+    model_40 = "gpt-4-1106-preview"
 
-def request_to_chatgpt(prompts: List[Any], question: str):
+def request_to_chatgpt_35(prompts: List[Any], question: str):
     prompts.append({"role": "user", "content": question})
     try:
-        res = client.chat.completions.create(
-            model=model, 
+        res = client_35.chat.completions.create(
+            model=model_35, 
             messages=prompts,
             temperature=0.7,
             # max_tokens=800,
@@ -36,3 +44,21 @@ def request_to_chatgpt(prompts: List[Any], question: str):
         logger.error(e)
         return (False, str(e), None)
 
+def request_to_chatgpt_40(prompts: List[Any], question: str):
+    prompts.append({"role": "user", "content": question})
+    try:
+        res = client_40.chat.completions.create(
+            model=model_40,
+            messages=prompts,
+            temperature=0.7,
+            # max_tokens=800,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+        usage = res.usage.total_tokens
+        return (True, res.choices[0].message.content, usage)
+    except Exception as e:
+        logger.error(e)
+        return (False, str(e), None)
