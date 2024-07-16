4bc173ff46577f59dbcfe5a22ffd6a1786466a80
fengsh <shaohong.feng.78@gmail.com>
2024-07-16 09:51:31 -0400

    update version

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from components.main_tab import main_tab
 from components.html_table_tab import html_tab
 from extractor.stampers import ArticleStamper
 
-__version__ = "0.1.31"
+__version__ = "0.1.32"
 
 def initialize():
     # prepare logger
