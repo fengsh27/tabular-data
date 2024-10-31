5ac872a668910c3e73f4c28ef829c54a5caac377
shaohong feng <shaohong.feng.78@gmail.com>
2024-10-31 11:01:29 -0400

    employ semantic similarity when comparing text

--- benchmark/evaluate.py
+++ benchmark/evaluate.py
@@ -8,7 +8,10 @@ import math
 import functools
 import logging
 
-from extractor.utils import extract_float_value
+from extractor.utils import (
+    extract_float_value, 
+    extract_float_values,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -24,18 +27,78 @@ Drug Name:      15/100
 """
 
 DELTA_VALUE = 0.000001
+SIMILARITY_DISTANCE_THRESHOLD = 0.5
 
+def is_abbreviation_or_contraction(a: str, b: str):
+    if a is None or b is None:
+        return False
+    a = a.strip()
+    b = b.strip()
+    if len(a) == 0 and len(b) == 0:
+        return True
+    if len(a) == 0 or len(b) == 0:
+        return False
+    short = a.lower() if len(a) < len(b) else b.lower()
+    long = a.lower() if len(a) >= len(b) else b.lower()
+    start_ix = 0
+    matches = False
+    for c in short:
+        if not ((c >= 'a' and c <= 'z') or (c>= 'A' and c <= 'Z')):
+            continue
+        ix = long.find(c, start_ix)
+        if ix < start_ix:
+            return False
+        matches = True
+        start_ix = ix + 1
+    return matches
+
+def is_values_in_strings_equaled(a: str, b: str):
+    if a is None and b is None:
+        return True
+    if a is None or b is None:
+        return False
+    vals1 = extract_float_values(a)
+    vals2 = extract_float_values(b)
+    if vals1 is None and vals2 is None:
+        return True
+    if vals1 is None or vals2 is None:
+        return False
+    if len(vals1) != len(vals2):
+        return False
+    for ix in range(len(vals1)):
+        if vals1[ix] != vals2[ix]:
+            return False
+    return True
+
+class TextComparer:
+    def __init__(self) -> None:
+        from sentence_transformers import SentenceTransformer, util
+        self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    
+    def compare(self, a, b) -> float:
+        if is_abbreviation_or_contraction(a, b):
+            return 1.0
+        if not is_values_in_strings_equaled(a, b):
+            return 0.0
+
+        from sentence_transformers import util
+        embedding_1 = self.model.encode(a, convert_to_tensor=True)
+        embedding_2 = self.model.encode(b, convert_to_tensor=True)
+        dist = util.pytorch_cos_sim(embedding_1, embedding_2)
+        return dist[0][0].item()
+    
 class TablesEvaluator:
     def __init__(self, rating_cols: List[str], anchor_cols: List[str]):
         self.rating_cols = rating_cols
         self.anchor_cols = anchor_cols
+        self.text_cmpr = TextComparer()
    
     def anchor_row(self, row1: Series, row2: Series):
         # locate row by anchor columns
         for c in self.anchor_cols:
             v1 = row1[c]
             v2 = row2[c]
-            if not TablesEvaluator.is_equal(v1, v2):
+            if not self.is_equal(v1, v2):
                 return False        
         return True
 
@@ -45,7 +108,7 @@ class TablesEvaluator:
             v1 = row1[c]
             v2 = row2[c]
             
-            if TablesEvaluator.is_equal(v1, v2):
+            if self.is_equal(v1, v2):
                 sum += 1
     
         return (int)(10.0 * sum / float(len(self.rating_cols)))
@@ -65,12 +128,13 @@ class TablesEvaluator:
         except ValueError:
             return None
 
-    @staticmethod
-    def is_equal(v1, v2) -> bool:
+    def is_equal(self, v1, v2) -> bool:
         if v1 == v2:
             return True
         if isinstance(v1, str) and isinstance(v2, str):
-            return v1.strip().lower() == v2.strip().lower()
+            dist = self.text_cmpr.compare(v1, v2)
+            # output_msg(f"{dist} = [{v1}] - [{v2}]")
+            return dist >= SIMILARITY_DISTANCE_THRESHOLD # v1.strip().lower() == v2.strip().lower()
         if not isinstance(v1, str) and not isinstance(v2, str):
             if math.isnan(v1) and math.isnan(v2):
                 return True
@@ -111,7 +175,7 @@ class TablesEvaluator:
                 continue
             similar_rows = []
             for r in candidate_rows:
-                if TablesEvaluator.is_equal(v, r[c]):
+                if self.is_equal(v, r[c]):
                     similar_rows.append(r)
             if len(similar_rows) == 0:
                 candidate_rows = rows
@@ -121,12 +185,6 @@ class TablesEvaluator:
                 candidate_rows = similar_rows
         return None if len(similar_rows) == 0 else similar_rows[0]
 
-
-    def anchor_row_from_dataframe(self, row: Series, df: DataFrame):
-        for ix, r in df.iterrows():
-            if self.anchor_row(r, row):
-                return r
-        return None
     
     def rate_rows(self, baseline: DataFrame, target: DataFrame) -> int:
         bshape = baseline.shape
@@ -154,5 +212,4 @@ class TablesEvaluator:
     
     def compare_tables(self, baseline:pd.DataFrame, target:pd.DataFrame) -> tuple[int, int]:
         return self.rate_rows(baseline, target)
-
 
---
+++ tests/test_benchmark_evalulator.py
@@ -0,0 +1,33 @@
+import pytest
+import pandas as pd
+
+from benchmark.evaluate import TablesEvaluator
+from benchmark.test_benchmark_with_semantic import (
+    PK_ANCHOR_COLUMNS, 
+    PK_RATING_COLUMNS
+)
+
+def test_anchro_row_from_rows():
+    pmid = "30950674"
+    target = pd.read_csv(f"./benchmark/data/{pmid}-pk-summary-gpt4o.csv")
+    baseline = pd.read_csv(f"./benchmark/data/{pmid}-pk-summary-baseline.csv")
+
+    bshape = baseline.shape
+    tshape = target.shape
+    if bshape[1] != tshape[1]:
+        return 0
+        
+    less = baseline if bshape[0] <= tshape[0] else target
+    much = baseline if bshape[0] > tshape[0] else target
+    
+    much_rows = much.to_dict('records')
+    
+    evaluator = TablesEvaluator(
+        anchor_cols=PK_ANCHOR_COLUMNS, 
+        rating_cols=PK_RATING_COLUMNS
+    )
+    for i, r in less.iterrows():
+        the_row = evaluator.anchor_row_from_rows(r, much_rows)
+        assert the_row is not None
+
+
