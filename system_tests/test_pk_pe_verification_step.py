6d6f6d867b0d10e4e099ec1f1f26d4c56fe9b6cc
fengsh <shaohong.feng.78@gmail.com>
2025-12-12 09:39:55 -0500

    test PKPEVerificationStep with gpt-oss

--- system_tests/test_pk_pe_verification_step.py
+++ system_tests/test_pk_pe_verification_step.py
@@ -4,6 +4,7 @@ import pytest
 from extractor.agents.pk_pe_agents.pk_pe_verification_step import PKPECuratedTablesVerificationStep
 from extractor.agents.pk_pe_agents.pk_pe_correction_step import PKPECuratedTablesCorrectionStep
 from extractor.agents.pk_pe_agents.pk_pe_agents_types import PKPECurationWorkflowState
+from extractor.request_gpt_oss import get_gpt_oss
 
 @pytest.mark.skip()
 def test_pk_pe_verification_step(
@@ -21,6 +22,7 @@ def test_pk_pe_verification_step(
     )
 
     state: PKPECurationWorkflowState = {
+        "llm": llm,
         "paper_title": paper_title_17635501,
         "paper_abstract": paper_abstract_17635501,
         "source_tables": md_table_17635501_table_3,
@@ -30,7 +32,8 @@ def test_pk_pe_verification_step(
     state = step.execute(state)
 
     assert state["curated_table"] is not None
-    
+
+@pytest.mark.skip()
 def test_pk_pe_verification_step_on_29100749(
     llm,
     step_callback,
@@ -51,11 +54,49 @@ def test_pk_pe_verification_step_on_29100749(
         domain="pharmacokinetic population and individual",
     )
     state: PKPECurationWorkflowState = {
+        "llm": llm,
         "paper_title": title_29100749,
         "paper_abstract": abstract_29100749,
         "source_tables": source_table_29100749_table_2,
         "curated_table": curated_table_29100749,
         "step_output_callback": step_callback,
+    }
+    state = verification_step.execute(state)
+    n = 0
+    while n < 5 and not ("final_answer" in state and state["final_answer"]):
+        state = correction_step.execute(state)
+        state = verification_step.execute(state)
+        n += 1
+    assert state["curated_table"] is not None
+
+def test_pk_pe_verification_step_on_29100749_with_gpt_oss(
+    # llm,
+    step_callback,
+    pmid_db,
+    title_29100749,
+    abstract_29100749,
+    source_table_29100749_table_2,
+    curated_table_29100749,
+):
+    llm = get_gpt_oss()
+    verification_step = PKPECuratedTablesVerificationStep(
+        llm=llm,
+        pmid="29100749",
+        domain="pharmacokinetic population and individual",
+    )
+    correction_step = PKPECuratedTablesCorrectionStep(
+        llm=llm,
+        pmid="29100749",
+        domain="pharmacokinetic population and individual",
+    )
+    state: PKPECurationWorkflowState = {
+        "llm": llm,
+        "paper_title": title_29100749,
+        "paper_abstract": abstract_29100749,
+        "source_tables": [source_table_29100749_table_2],
+        "curated_table": curated_table_29100749,
+        "step_output_callback": step_callback,
+        "step_count": 0,
     }
     state = verification_step.execute(state)
     n = 0
